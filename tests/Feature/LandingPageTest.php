50ff98e1de833902a1f3b4db9c6c5518a91f9589
lew <jaysonpinggoy11@gmail.com>
2026-07-16 10:41:11 +0800

    Initial commit for HIMS landing page

---
+++ tests/Feature/LandingPageTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class LandingPageTest extends TestCase
+{
+    public function test_homepage_displays_supply_chain_and_inventory_management_heading(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+        $response->assertSee('Supply Chain & Inventory Management');
+    }
+}
